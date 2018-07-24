254d4a966cfac19d3be3f418950b6bc94405bf34
ma- <ponyonekoak@gmail.com>
2018-07-24 20:17:42 +0900

    テスト用にデプロイ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
   # POST
   def create
-    @user_with_this_email = User.find_by(email: params[:email])
-    @result = false
-    if (@user_with_this_email == nil)
-      @user = User.new
-      @user.email = params[:email]
-      @user.password = params[:password]
-      @result = @user.save
-    end
-    render json: { result: @result }, status: :ok
+=begin
+@user_with_this_email = User.find_by(email: params[:email])
+@result = false
+if (@user_with_this_email == nil)
+  @user = User.new
+  @user.email = params[:email]
+  @user.password = params[:password]
+  @result = @user.save
+end
+render json: { result: @result }, status: :ok
+=end
+  render json: { test: 'test'}, status: :ok
+  end
+
+  def index
+    render json: { test: 'test'}, status: :ok
   end
 
 end
